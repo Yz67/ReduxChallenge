203153a2f7d8062bcd75e5e8bdef4131ef9544b5
Brock <bws219@lehigh.edu>
2017-10-18 16:13:19 -0700

    project complete

--- reactApp/components/HourSlot.js
+++ reactApp/components/HourSlot.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { connect } from 'react-redux';
 
 class HourSlot extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      open: false
+      open: false,
+      name: this.props.call.name || '',
+      number: this.props.call.number || ''
     }
   }
 
-  openModal() {
+  toggleModal() {
     this.setState({open: !this.state.open});
   }
 
@@ -17,27 +20,59 @@ class HourSlot extends React.Component {
     return {
       overlay: {
         top: 200,
-        left: 400,
-        right: 400,
+        left: 200,
+        right: 200,
         bottom: 200,
       }
     }
   }
 
+  handleNameChange(e) {
+    this.setState({ name: e.target.value })
+  }
+
+  handleNumChange(e) {
+    this.setState({ number: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.update(this.props.position, this.state.name, this.state.number);
+    this.toggleModal();
+  }
+
+  setStyle() {
+    if (this.props.call.name || this.props.call.number) {
+      return { backgroundColor: 'palegreen' }
+    } else {
+      return {}
+    }
+  }
+
   render() {
     return (
-      <div className='row grid-block' onClick={() => this.openModal()}>
-        <p>{this.props.call.name}</p>
-        <p>{this.props.call.number}</p>
+      <div className='col grid-block' style={this.setStyle()}
+        onClick={() => this.toggleModal()}>
+        <div>{this.props.call.name}</div>
+        <div>{this.props.call.number}</div>
         <Modal style={this.modalStyle()} isOpen={this.state.open}>
-          <form className='col'>
+          <form onSubmit={(e) => this.handleSubmit(e)} className='col'>
             <h3>Add/Edit Call</h3>
             <h5>Who you gonna call?</h5>
-            <input type='text' name='name' placeholder='Name' />
-            <input type='text' name='number' placeholder='Phone Number' />
+            <input value={this.state.name}
+              autoFocus={true}
+              onChange={(e) => this.handleNameChange(e)}
+              type='text' name='name' placeholder='Name'
+            />
+            <input value={this.state.number}
+              onChange={(e) => this.handleNumChange(e)}
+              type='text' name='number' placeholder='Phone Number'
+            />
             <div className='row'>
-              <input type='submit' value='save' />
-              <button>Cancel</button>
+              <input type='submit' value='Save' />
+              <input type='button' value='Cancel'
+                onClick={() => this.toggleModal()}
+              />
             </div>
           </form>
         </Modal>
@@ -46,8 +81,15 @@ class HourSlot extends React.Component {
   }
 }
 
-// const mapStateToProps = () => ({});
-//
-// const mapDispatchToProps = dispatch => ({});
+const mapStateToProps = () => ({});
 
-export default HourSlot;
+const mapDispatchToProps = dispatch => ({
+  update: (position, name, number) => dispatch({
+    type: 'UPDATE',
+    position,
+    name,
+    number,
+  })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HourSlot);
--- reactApp/reducers/mainReducer.js
+++ reactApp/reducers/mainReducer.js
@@ -4,6 +4,17 @@ const constructState = () => (
 
 const reducer = (state = constructState(), action) => {
   switch (action.type) {
+    case 'UPDATE':
+      let copy = state.map(day => (
+        day.map(hour => (
+          Object.assign({}, hour)
+        ))
+      ));
+      copy[action.position.day][action.position.hour] = {
+        name: action.name,
+        number: action.number
+      };
+      return copy;
     default:
       return state;
   }
