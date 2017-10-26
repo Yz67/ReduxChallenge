d0ec5f41a18e5c1680693d9f39e08f3d22af401c
Graham Smith <smitgw11@gmail.com>
2017-10-26 15:35:40 -0700

    movePlayer exercise

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './components/App';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+import Board from './components/Board';
 /* WHEN YOU ACTUALLY WRITE YOUR REDUCER, FIX THE 2 LINES BELOW */
 // import mainReducer from './reducers/mainReducer'; /*UNCOMMENT*/
-const mainReducer = (state = 5) => state; /*REMOVE*/
+// const mainReducer = (state = 5) => state; /*REMOVE*/
 
-const store = createStore(mainReducer);
+// const store = createStore(mainReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // <Provider store={store}>
+    <Board />,
+  // </Provider>,
   document.getElementById('root')
 );
---
+++ reactApp/components/Square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button onClick={this.props.whenClicked} style={{ backgroundColor: this.props.color }} className="square"></button>
+    )
+  }
+}
+
+export default Square;
